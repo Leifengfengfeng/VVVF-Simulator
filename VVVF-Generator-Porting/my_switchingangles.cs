0131b2a3682db563a458584c7583f31fa854b3e8
JOTAN <77259842+JOTAN-0655@users.noreply.github.com>
2022-01-31 14:20:45 +0900

    moved enum VVVF_Sound_Names

--- VVVF-Generator-Porting/my_switchingangles.cs
+++ VVVF-Generator-Porting/my_switchingangles.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VVVF_Generator_Porting
+﻿namespace VVVF_Generator_Porting
 {
     public class my_switchingangles
     {
