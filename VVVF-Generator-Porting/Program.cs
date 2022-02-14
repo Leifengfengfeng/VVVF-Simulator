617680a73aa86ce4c6a00085eb5c931d77e2453c
JOTAN <77259842+JOTAN-0655@users.noreply.github.com>
2022-02-14 12:33:06 +0900

    Program.cs,Index program change. Generate Env add

--- VVVF-Generator-Porting/Program.cs
+++ VVVF-Generator-Porting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static VVVF_Generator_Porting.vvvf_sound_definition;
+
 using static VVVF_Generator_Porting.Generation.Generate_Control_Info;
 using static VVVF_Generator_Porting.Generation.Generate_Hexagon;
 using static VVVF_Generator_Porting.Generation.Generate_RealTime;
@@ -28,63 +29,89 @@ namespace VVVF_Generator_Porting
         {
             Console.WriteLine("Select the function to do. (Use \",\" to use multiple function)");
 
-            Console.WriteLine("-SOUND RELATE");
-            Console.WriteLine("1 : Generate Sound");
-            Console.WriteLine();
+            String[] index = new string[]
+            {
+                "-SOUND RELATE",
+                "Generate Sound",
+                "Generate Environment Sound",
 
-            Console.WriteLine("-WAVE FORM RELATE");
-            Console.WriteLine("2 : Generate Wave form Video");
-            Console.WriteLine("3 : Generate U V W Wave form Video");
-            Console.WriteLine("4 : Generate Taroimo like Wave form Video");
-            Console.WriteLine();
+                "-WAVE FORM RELATE",
+                "Generate Wave form Video",
+                "Generate U V W Wave form Video",
+                "Generate Taroimo like Wave form Video",
 
-            Console.WriteLine("-HEXAGON RELATE");
-            Console.WriteLine("5 : Generate Hexagon");
-            Console.WriteLine("6 : Generate Taroimo like Hexagon");
-            Console.WriteLine("7 : Generate Hexagon Explain");
-            Console.WriteLine("8 : Generate Hexagon Image");
-            Console.WriteLine();
+                "-HEXAGON RELATE",
+                "Generate Hexagon",
+                "Generate Taroimo like Hexagon",
+                "Generate Hexagon Explain",
+                "Generate Hexagon Image",
 
-            Console.WriteLine("-OTHER RELATE");
-            Console.WriteLine("9 : Generate Mascon Video");
-            Console.WriteLine("10 : Generate Taroimo like Mascon Video");
-            Console.WriteLine("11 : Realtime VVVF Sound generation");
-            Console.WriteLine();
+                "-MASCON RELATE",
+                "Generate Mascon Video",
+                "Generate Taroimo like Mascon Video",
+
+                "-OTHERS",
+                "Realtime VVVF Sound generation",
+            };
+
+            int count = 1;
+            for(int i = 0; i < index.Length; i++)
+            {
+                String index_x = index[i];
+                if (index_x.StartsWith("-"))
+                {
+                    if (count != 1) Console.WriteLine();
+                    Console.WriteLine(index_x);
+                }
+                else
+                {
+                    Console.WriteLine(count.ToString() + " : " + index_x);
+                }
+
+                count++;
+                    
+            }
 
             String line = Console.ReadLine();
 
             String[] split = line.Split(",");
 
-            bool gen_audio = false;
+            bool gen_audio = false, gen_env_audio = false;
             bool gen_U_V = false, gen_UVW = false, gen_U_V_taroimo = false;
             bool gen_hexagon = false , gen_hexagon_taroimo = false, gen_hexagon_explain = false, gen_hexagon_image = false;
             bool gen_mascon_video = false , gen_mascon_taroimo_video = false;
             bool realtime = false;
 
+            int c = 1;
             for(int i = 0; i < split.Length; i++)
             {
-                if (split[i] == "1") gen_audio = true;
-                if (split[i] == "2") gen_U_V = true;
-                if (split[i] == "3") gen_UVW = true;
-                if (split[i] == "4") gen_U_V_taroimo = true;
-                if (split[i] == "5") gen_hexagon = true;
-                if (split[i] == "6") gen_hexagon_taroimo = true;
-                if (split[i] == "7") gen_hexagon_explain = true;
-                if (split[i] == "8") gen_hexagon_image = true;
-                if (split[i] == "9") gen_mascon_video = true;
-                if (split[i] == "10") gen_mascon_taroimo_video = true;
-                if (split[i] == "11") realtime = true;
+                if (split[i] == c++.ToString()) gen_audio = true;
+                if (split[i] == c++.ToString()) gen_env_audio = true;
+
+                if (split[i] == c++.ToString()) gen_U_V = true;
+                if (split[i] == c++.ToString()) gen_UVW = true;
+                if (split[i] == c++.ToString()) gen_U_V_taroimo = true;
+
+                if (split[i] == c++.ToString()) gen_hexagon = true;
+                if (split[i] == c++.ToString()) gen_hexagon_taroimo = true;
+                if (split[i] == c++.ToString()) gen_hexagon_explain = true;
+                if (split[i] == c++.ToString()) gen_hexagon_image = true;
+
+                if (split[i] == c++.ToString()) gen_mascon_video = true;
+                if (split[i] == c++.ToString()) gen_mascon_taroimo_video = true;
+
+                if (split[i] == c++.ToString()) realtime = true;
             }
 
             
             if(gen_audio || gen_U_V || gen_mascon_video || gen_UVW || gen_hexagon || gen_hexagon_explain || gen_hexagon_taroimo || gen_mascon_taroimo_video
-                || gen_U_V_taroimo || gen_hexagon_image)
+                || gen_U_V_taroimo || gen_hexagon_image || gen_env_audio)
             {
                 VVVF_Sound_Names sound_name = get_Choosed_Sound();
                 String output_path = get_Path();
 
                 if (gen_audio) generate_sound(output_path, sound_name);
-
+                if (gen_env_audio) generate_env_sound(output_path, sound_name);
 
                 if (gen_U_V) generate_wave_U_V(output_path, sound_name);
                 if (gen_UVW) generate_wave_UVW(output_path, sound_name);
